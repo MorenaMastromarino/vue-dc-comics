b154f0b10afe5184573cc190b65c4dab8705091f
MorenaMastromarino <morena.mastromarino98@gmail.com>
2021-11-25 13:28:40 +0100

    jumbotron - stampo card delle serie con ciclo

---
+++ src/components/SerieCard.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <div class="serie-card">
+    <img :src="cardImage" :alt="cardTitle">
+    <h3>{{cardTitle}}</h3>      
+  </div> 
+  
+</template>
+
+<script>
+export default {
+  name: 'SerieCard',
+  props: {
+    cardImage: String,
+    cardTitle: String,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .serie-card {    
+    color: #fff;
+    flex-basis: calc(100% / 6);
+  }
+</style>
